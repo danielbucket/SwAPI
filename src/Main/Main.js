2cc342e2a659b6820a54309e64c84568d6a8a57d
Dave W <dviper@gmail.com>
2017-07-03 14:28:41 -0600

    complete OpeningCrawl component styling, add infinite text scroll

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './Main.css'
 
 const Main = () => {
 
   return (
-    <div>
+    <div className="main">
       Main Component
     </div>
   )
--- src/OpeningCrawl/OpeningCrawl.js
+++ src/OpeningCrawl/OpeningCrawl.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import './OpeningCrawl.css'
 
 const OpeningCrawl = ({ opening }) => {
 
   return (
-    <div>
-      { opening }
+    <div className="opening-crawl">
+      <div className="crawl-text">
+        <h1 className="movie-title">
+          A New Hope
+        </h1>
+        <h4 className="release-year">
+          (1977)
+        </h4>
+        <p>
+          { opening }
+        </p>
+      </div>
     </div>
   )
 }
