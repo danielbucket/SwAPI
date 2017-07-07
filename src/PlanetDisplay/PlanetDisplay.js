3f9132ce63a9ba52b81a86fed9262171ad48c684
Dave W <dviper@gmail.com>
2017-07-06 19:30:57 -0600

    refactor the display planets functionality

--- src/PlanetDisplay/PlanetDisplay.js
+++ src/PlanetDisplay/PlanetDisplay.js
@@ -4,13 +4,24 @@ const PlanetDisplay = ({ displayCards }) => {
 
   const planetCards = displayCards.map(i => {
 
+    let residentsList = i.residents.map((resi, index) => {
+      return (
+        <li key={resi}>{resi}</li>
+      )
+    })
+
+    if(residentsList.length === 0) {
+      residentsList = 'Uninhabited'
+    }
+
     return (
-      <div className="planet-card">
+      <div key={i.name} className="planet-card">
         <h1>Name: { i.name }</h1>
         <p>Population: { i.population }</p>
         <p>Climate: { i.climate }</p>
         <p>Terrain: { i.terrain }</p>
-        <p>Residents: { i.residents }</p>
+        <p>Residents:</p>
+        <ul>{ residentsList }</ul>
       </div>
     )
   })
