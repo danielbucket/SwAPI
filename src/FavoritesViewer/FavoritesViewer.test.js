af3a12848752bc20739b459f2cef2e1703f5e593
Daniel Bucket <danieljbucket@gmail.com>
2017-07-08 14:01:36 -0600

    testing for FavoritesViewer completed

--- src/FavoritesViewer/FavoritesViewer.test.js
+++ src/FavoritesViewer/FavoritesViewer.test.js
@@ -1,12 +1,30 @@
 import { shallow, mount } from 'enzyme';
-import fetchMock from '';
-import FavoritesViewer from './FavoritesViewer';
+import React from 'react';
+import FavoritesViewer from './FavoritesViewer.js';
 
 describe( 'FavoritesViewer', () => {
   it('should take a shit', () => {
-    const FavoritesViewer = shallow(<FavoritesViewer />)
+    const wrapper = shallow(<FavoritesViewer />)
 
-    console.log(FavoritesViewer.fin('div'))
-    // expect(FavoritesViewer.find('div'))
+    expect(wrapper.find('div').length).toEqual(1)
+  })
+
+  it('should have props', () => {
+    const wrapper = shallow(<FavoritesViewer favoriteCount={ 10 }/>)
+    const favsCount = wrapper.find('.fav-count')
+
+    expect(favsCount.props().children).toEqual(10)
+  })
+
+  it('should do a thing', () => {
+    const wrapper = shallow(<FavoritesViewer />)
+
+    expect(wrapper.children().props().children.props.className).toBe('all-favs-button')
+  })
+
+  it('should be wrapped in a <Link to='/'> tag', () => {
+    const wrapper = shallow(<FavoritesViewer />)
+
+    expect(wrapper.find('Link').props().to).toEqual('/')
   })
 })
