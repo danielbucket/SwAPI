855bdfc30985b61baed033211348f48d071247fc
Daniel Bucket <danieljbucket@gmail.com>
2017-07-08 15:59:19 -0600

    testing for FilmDisplay

--- src/FilmDisplay/FilmDisplay.js
+++ src/FilmDisplay/FilmDisplay.js
@@ -6,17 +6,16 @@ const FilmDisplay = ({ displayCards, starItem }) => {
 
   const eachCard = sorted.map( i => {
     let favIcon = i.favorite ? '💩' : '🎃'
-
     return (
       <article  key={ i.episode_id }
-      className='film-card'>
+                className='film-card'>
         <div className="card-header">
-          <h1>Star Wars: Episode {i.episode_id}</h1>
+          <h1>Star Wars: Episode { i.episode_id }</h1>
           <span value={ i }
-            onClick={ () => { starItem(i, 'films') }}
-            className="fav-btn"
-            role='img'
-          aria-labelledby='add to favorites'>{favIcon}</span>
+                onClick={ () => { starItem(i, 'films') } }
+                className="fav-btn"
+                role='img'
+                aria-labelledby='add to favorites'>{ favIcon }</span>
         </div>
         <h3> { i.title } </h3>
         <p>Release Date: { i.release_date }</p>
---
+++ src/FilmDisplay/FilmDisplay.test.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import FilmDisplay from './FilmDisplay';
+
+describe('FilmDisplay', () => {
+  const filmStub = [{
+    characters: ["http://swapi.co/api/people/3/"],
+    created: "2014-12-19T16:52:55.740000Z",
+    director: "George Lucas",
+    edited: "2015-04-11T09:45:18.689301Z",
+    episode_id: 1,
+    favorite: false,
+    id: "be07b928-10e0-4aea-b125-e3ebd8177c61",
+    opening_crawl: "Turmoil has engulfed...",
+    planets: [],
+    producer: "Rick McCallum",
+    release_date : "1999-05-19",
+    species: [],
+    starships: [],
+    title: "The Phantom Menace",
+    url: "http://swapi.co/api/films/4/",
+    vehicles: [],
+    __proto__: {}
+  },
+  {
+    characters: ["http://swapi.co/api/people/4/"],
+    created: "2014-12-19T16:52:55.740000Z",
+    director: "George Lucas",
+    edited: "2015-04-11T09:45:18.689301Z",
+    episode_id: 2,
+    favorite: false,
+    id: "be07b928-10e0-4aea-b125-e3ebd8177c61",
+    opening_crawl: "Turmoil has engulfed...",
+    planets: [],
+    producer: "Rick McCallum",
+    release_date : "1999-05-19",
+    species: [],
+    starships: [],
+    title: "Harsh Realities",
+    url: "http://swapi.co/api/films/4/",
+    vehicles: [],
+    __proto__: {}
+  }
+]
+  const mockFunc = jest.fn()
+  const wrapper = mount(<FilmDisplay displayCards={ filmStub } starItem={ mockFunc }/>)
+
+  it('01: should...', () => {
+
+    expect(wrapper.find('.film-display').props().children[0].key).toEqual('1')
+    expect(wrapper.find('.film-display').props().children[0].props.children[1].props.children[1]).toEqual('The Phantom Menace')
+    expect(wrapper.find('span').first().props().children).toEqual('🎃')
+  })
+
+  it('02: should...', () => {
+    const favButton = wrapper.find('.fav-btn').first()
+
+    favButton.simulate('click')
+    expect(mockFunc).toHaveBeenCalledTimes(1)
+    favButton.simulate('click')
+    expect(mockFunc).toHaveBeenCalledTimes(2)
+  })
+
+  it('03: should...', () => {
+   expect(wrapper.find('.film-display').props().children[1].props.children[1].props.children[1]).toEqual('Harsh Realities')
+   expect(wrapper.find('.film-display').props().children.length).toEqual(2)
+  })
+})
